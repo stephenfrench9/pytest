ce8dd518be9df56640b59e202ea46cda6dbee004
Stephen French <stephen.french@invitae.com>
2021-05-27 09:24:11 -0700

    rename functions

--- fixture_with_module_scope/test_module_1.py
+++ fixture_with_module_scope/test_module_1.py
@@ -1,8 +1,8 @@
 def test_1__module_1(module_file):
-    print("test_1: ", module_file.read())
+    print("test_1__module_1", module_file.read())
     assert 0
 
 
 def test_2__module_1(module_file):
-    print("test_2: ", module_file.read())
+    print("test_2__module_1: ", module_file.read())
     assert 0
--- fixture_with_module_scope/test_module_2.py
+++ fixture_with_module_scope/test_module_2.py
@@ -1,4 +1,4 @@
 def test_1__module_2(module_file):
-    print("test_4: ", module_file.read())
+    print("test_1__module_2: ", module_file.read())
     assert 0
 
