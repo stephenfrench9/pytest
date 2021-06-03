cf9d3520bbfd14ab224c23068502986f0e07d47b
Stephen French <stephen.french@invitae.com>
2021-06-03 12:34:31 -0700

    basic autouse example

---
+++ autouse/test_1.py
@@ -0,0 +1,28 @@
+# https://docs.pytest.org/en/6.2.x/fixture.html#autouse-fixtures-fixtures-you-don-t-have-to-request
+
+import pytest
+
+
+@pytest.fixture
+def string0():
+    return "a"
+
+
+@pytest.fixture
+def list0():
+    return []
+
+
+@pytest.fixture(autouse=True)
+def append0(list0, string0):
+    return list0.append(string0)
+
+
+def test_1(list0, string0):
+    assert list0 == [string0]
+
+
+def test_2(list0, string0):
+    list0.append(2)
+    assert list0 == [string0, 2]
+
